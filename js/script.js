a81ccd40bef4d05eb3387cbc2751f1600a3a3736
Ida Darmochwał <ida.mdart@gmail.com>
2021-04-15 09:57:17 +0200

    Edit indentation in html

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
 'use strict';
 
+{
+  const titleClickHandler = function(event) {
+    event.preventDefault();
 
-const titleClickHandler = function(event) {
-  event.preventDefault();
+    const clickedElement = this;
 
-  const clickedElement = this;
+    console.log('Link was clicked!');
+    console.log(event);
 
-  console.log('Link was clicked!');
-  console.log(event);
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* [DONE] remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks) {
+      activeLink.classList.remove('active');
+    }
 
-  for(let activeLink of activeLinks) {
-    activeLink.classList.remove('active');
-  }
+    /* [DONE] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
 
-  /* [DONE] add class 'active' to the clicked link */
-  console.log('clickedElement:', clickedElement);
-  clickedElement.classList.add('active');
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
-  /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active');
+    for(let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
 
-  for(let activeArticle of activeArticles) {
-    activeArticle.classList.remove('active');
-  }
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
-  /* [DONE] get 'href' attribute from the clicked link */
-  const articleSelector = clickedElement.getAttribute('href');
-  console.log(articleSelector);
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
-  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector(articleSelector);
-  console.log(targetArticle);
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 
-  /* [DONE] add class 'active' to the correct article */
-  targetArticle.classList.add('active');
-
-
+  };
 
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
@@ -97,45 +97,43 @@ const titleClickHandler = function(event) {
 
 
   function generateTags(){
-  /* find all articles */
+    /* find all articles */
 
-  
-
-  /* START LOOP: for every article: */
+    /* START LOOP: for every article: */
 
 
-    /* find tags wrapper */
+      /* find tags wrapper */
 
 
-    /* make html variable with empty string */
+      /* make html variable with empty string */
 
 
-    /* get tags from data-tags attribute */
+      /* get tags from data-tags attribute */
 
 
-    /* split tags into array */
+      /* split tags into array */
 
 
-    /* START LOOP: for each tag */
+      /* START LOOP: for each tag */
 
 
-      /* generate HTML of the link */
+        /* generate HTML of the link */
 
 
-      /* add generated code to html variable */
+        /* add generated code to html variable */
 
 
-    /* END LOOP: for each tag */
+      /* END LOOP: for each tag */
 
 
-  /* insert HTML of all the links into the tags wrapper */
+    /* insert HTML of all the links into the tags wrapper */
 
 
-  /* END LOOP: for every article: */
+    /* END LOOP: for every article: */
 
 
   }
 
   generateTags();
 
-};
+}

790ba3b97f2c9c600b6ed951c221831b3f15e97b
Ida Darmochwał <ida.mdart@gmail.com>
2021-04-21 16:11:11 +0200

    Edit styles of .list-horizontal & delete number of tags occurance in allTagsHTML in js

--- js/script.js
+++ js/script.js
@@ -209,7 +209,7 @@
     /* START LOOP: for each tag in allTags: */
     for(let tag in allTags){
       /* generate code of a link and add it to allTagsHTML */
-      allTagsHTML += `<li><a href=#tag-${tag} class="${calculateTagClass(allTags[tag], tagsParams)}">${tag} (${allTags[tag]})</a></li>`;
+      allTagsHTML += `<li><a href=#tag-${tag} class="${calculateTagClass(allTags[tag], tagsParams)}">${tag}</a></li>`;
       console.log(`allTagsHTML: `, allTagsHTML);
     }
     /* END LOOP: for each tag in allTags: */
